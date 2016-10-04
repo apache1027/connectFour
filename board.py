7ae0ec8299ecd166960cac4ebe52c20cb7c42f09
apache1027 <m173750@usna.edu>
2016-10-04 14:01:30 -0700

    removed test

--- board.py
+++ board.py
@@ -1,7 +1,6 @@
 class Board(object):
     def __init__(self):
         #self explanatory fields and intilize board
-        #this is a test commment
         self.board = []
         self.player = 'R'
         self.height = 6
