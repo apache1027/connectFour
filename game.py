64c67bfa1b27e77ddc5bccaa484dd1618d88fa66
apache1027 <m173750@usna.edu>
2016-10-05 09:43:22 -0400

    almost done transferring to game.py

--- game.py
+++ game.py
@@ -18,40 +18,115 @@ class Board(object):
             print " ".join(row)
         for i in range(1, self.width + 1):
             print i,
+        print '\n'
 
     #Takes the current player tile and searches the board for 4 in a row
     def victory_test(self, cur_name):
         #check horizontal
         for x in range (self.height):
             for y in range (self.width - 3):
-                if self.board[x][y] == self.player and self.board[x][y+1] == self.player and self.board[x][y+2] == self.player and self.board[x][y+3] == self.player:
+                if self.board[x][y] == cur_name and self.board[x][y+1] == cur_name and self.board[x][y+2] == cur_name and self.board[x][y+3] == cur_name:
                     self.print_board()
                     print '\n' ,cur_name, "player wins!"
                     return True
         #check vertical
         for x in range (self.height - 3):
             for y in range (self.width):
-                if self.board[x][y] == self.player and self.board[x + 1][y] == self.player and self.board[x + 2][y] == self.player and self.board[x + 3][y] == self.player:
+                if self.board[x][y] == cur_name and self.board[x + 1][y] == cur_name and self.board[x + 2][y] == cur_name and self.board[x + 3][y] == cur_name:
                     self.print_board()
                     print '\n' ,cur_name, "player wins!"
                     return True
         #check forward diagonal \
-        for x in range (self.height - 3):
-            for y in range (self.width - 3):
-                if self.board[x][y] == self.player and self.board[x + 1][y + 1] == self.player and self.board[x + 2][y + 2] == self.player and self.board[x + 3][y + 3] == self.player:
-                    self.print_board()
+                for x in range (self.height - 3):
+                    for y in range (self.width - 3):
+                        if self.board[x][y] == cur_name and self.board[x + 1][y + 1] == cur_name and self.board[x + 2][y + 2] == cur_name and self.board[x + 3][y + 3] == cur_name:
+                            self.print_board()
                     print '\n' ,cur_name, "player wins!"
                     return True
         #check back diagonal /
         print '\n'
         for x in range (self.height - 3):
             for y in range (3, self.width):
-                if self.board[x][y] == self.player and self.board[x + 1][y - 1] == self.player and self.board[x + 2][y - 2] == self.player and self.board[x + 3][y - 3] == self.player:
+                if self.board[x][y] == cur_name and self.board[x + 1][y - 1] == cur_name and self.board[x + 2][y - 2] == cur_name and self.board[x + 3][y - 3] == cur_name:
                     self.print_board()
                     print '\n' ,cur_name, "player wins!"
                     return True
-                
+
         return False
 
+    #check board for tie
+    def tie_test(self):
+        return True
+
+    #update the board
+    def update_board(self, val, move):
+        self.board[self.cur_row[move]][move] = val
+        self.cur_row[move] = self.cur_row[move]-1
+
+#will make sure the user inputs a valid move
+def check_move(user_move):
+    try:
+        user_move = int(user_move)
+    except ValueError:
+        return False
+    else:
+        if(user_move < 1 or user_move > board.width):
+            return False
+        if(board.cur_row[user_move - 1] < 0):
+            return False
+    return True
+
+#human turn
+def human_turn(board, human, human_name):
+    board.print_board()
+    user_move = raw_input("%s player, enter a number between 1 and %d: " %(human_name, board.width))
+    while(not check_move(user_move)):
+        user_move = raw_input("%s player, enter a number between 1 and %d: " %(human_name, board.width))
+    user_move = int(user_move)
+    user_move -= 1
+    board.update_board(human, user_move)
+
+
 board = Board()
-board.print_board()
+turn = 'R'
+cont = True
+#get user input for who goes first
+#set human and computer to respective value
+temp = 'a'
+while (temp != 'c' and temp != 'h'):
+    temp = raw_input("Will the (c)omputer or the (h)uman go first?: ")
+if(temp == 'c'):
+    computer = 'R'
+    human = 'B'
+    human_name = "Black"
+    computer_name = "Red"
+else:
+    computer = 'B'
+    human = 'R'
+    human_name = "Red"
+    computer_name = "Black"
+test = True
+while(test):
+    temp = raw_input("Enter the search depth: ")
+    try:
+        int(temp)
+    except ValueError:
+        test = True
+    else:
+        temp = int(temp)
+        test = False
+depth = temp
+
+#loop until there is a winner
+while (cont):
+    if(turn == computer):
+        comp_turn(board, computer, computer_name)
+    else:
+        human_turn(board, human, human_name)
+    if(board.victory_test(turn)):
+        cont = False
+    if(turn == 'B'):
+        turn = 'R'
+    else:
+        turn = 'B'
+
